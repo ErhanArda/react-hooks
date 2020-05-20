6d572155a3fed83e4fc7cb1c44d86a3c30fae57e
Erhan Arda <erhanarda7@gmail.com>
2020-05-21 00:49:21 +0300

    useEffect usage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./App.css"
 
 const App = () => {
 
   const [theme, setTheme] = useState("light")
-  
+  const [num, setNum] = useState(0)
+
+  // componentDidMount(){
+  //   document.title = "Basic Hook Usage"
+  // }
+  useEffect(() => {
+    document.title = "Basic Hook Usage"
+  }, [])
+
   return (
     <div className={theme}>
-      <button onClick={()=>{setTheme("light")}}>Light</button>
-      <button onClick={()=>{setTheme("dark")}}>Dark</button>
+      <div >
+        <button onClick={() => { setTheme("light") }}>Light</button>
+        <button onClick={() => { setTheme("dark") }}>Dark</button>
+      </div>
+      <div>
+        <h1>{num}</h1>
+        <button onClick={() => { setNum(num + 1) }}>Increase</button>
+        <button onClick={() => { setNum(num - 1) }}>Decrease</button>
+        <button onClick={() => { setNum(0) }}>Reset</button>
+      </div>
     </div>
   )
 }
 
+export default App
 
 // class App extends Component {
 //   constructor(props) {
@@ -34,7 +51,3 @@ const App = () => {
 //     );
 //   }
 // }
-
-
-
-export default App
