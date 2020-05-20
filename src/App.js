117325354e2c934849f77b28a3b2edf1f51cf8ec
Erhan Arda <erhanarda7@gmail.com>
2020-05-21 00:07:04 +0300

    after using hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react'
 import "./App.css"
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: "light"
-    };
-  }
 
-  render() {
-    return (
-      <div className={this.state.theme} >
-        <button onClick={()=>{this.setState({
-          theme: "light"
-        })}} >Light</button>
-        <button onClick={()=>{this.setState({
-          theme: "dark"
-        })}}>Dark</button>
-      </div>
-    );
-  }
+const App = () => {
+
+  const [theme, setTheme] = useState("light")
+  
+  return (
+    <div className={theme}>
+      <button onClick={()=>{setTheme("light")}}>Light</button>
+      <button onClick={()=>{setTheme("dark")}}>Dark</button>
+    </div>
+  )
 }
 
-export default App;
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       theme: "light"
+//     };
+//   }
+//   render() {
+//     return (
+//       <div className={this.state.theme} >
+//         <button onClick={()=>{this.setState({
+//           theme: "light"
+//         })}} >Light</button>
+//         <button onClick={()=>{this.setState({
+//           theme: "dark"
+//         })}}>Dark</button>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+export default App
